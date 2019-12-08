367ed1975effa26c2cddd682916b8abb89905d29
linchang <1084677348@qq.com>
2019-12-08 14:23:49 +0800

    init

---
+++ Nodejs/path/index.js
@@ -0,0 +1,10 @@
+const path = require('path'); //引入该模块
+exports.sayHello = function() {};
+exports.downLoad = function() {};
+
+
+console.log(path);
+console.log(__dirname);
+console.log(__filename);
+console.log(exports);
+console.log(module);
