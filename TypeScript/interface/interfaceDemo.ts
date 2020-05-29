303dc9ca2fdd517bf620970ed32b6019075aa032
yolo <1084677348@qq.com>
2020-05-29 18:28:44 +0800

    [+]添加书签收藏夹

---
+++ TypeScript/interface/interfaceDemo.ts
@@ -0,0 +1,12 @@
+interface interfaceDemo {
+    weigth: Number;
+    age: Number;
+    name: String;
+}
+
+class Person implements interfaceDemo {
+    weigth = 178;
+    age = 25;
+    name = 'Jack';
+}
+
