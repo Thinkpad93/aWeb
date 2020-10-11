a333063c542ace3b562aaf066ce1002ae8a95208
ThinkPad <1084677348@qq.com>
2020-10-11 23:45:25 +0800

    [+]新增旧文件

---
+++ javaScript/es6/class-1.js
@@ -0,0 +1,20 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  //实例对象point自身的属性
+  toString() {
+    return "(" + this.x + "," + this.y + ")";
+  }
+}
+
+class Logger {
+  constructor() {
+    this.printName = (name = "there") => {
+      this.print(`Hello ${name}`);
+    };
+  }
+}
+
+const point = new Point(10, 20);
---
+++ javaScript/es6/titleCase.js
@@ -0,0 +1,8 @@
+function titleCase(str) {
+  const parts = str.trim().split(" ");
+  const trimmed = parts.filter((next) => next.length > 0);
+  const capitalized = trimmed.map((next) => {
+    return next[0].toUpperCase() + next.substring(1);
+  });
+  return capitalized.join(" ");
+}
